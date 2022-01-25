d784a6f6ecfa2cc6196455f648ed1025e2e43b90
prestondavid99 <buh.preston@gmail.com>
2022-01-25 10:03:24 -0700

    First Commit

---
+++ hangman/EvilHangman.java
@@ -0,0 +1,17 @@
+package hangman;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Set;
+
+public class EvilHangman {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        }
+
+
+    }
+
+
---
+++ hangman/EvilHangmanGame.java
@@ -0,0 +1,31 @@
+package hangman;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.SortedSet;
+
+public class EvilHangmanGame implements IEvilHangmanGame {
+    @Override
+    public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
+        Set<String> stringSet = null;
+
+        var scanner = new Scanner(dictionary);
+
+        while (scanner.hasNext()) {
+            String str = scanner.next();
+            stringSet.add(str);
+        }
+    }
+
+    @Override
+    public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
+        return null;
+    }
+
+    @Override
+    public SortedSet<Character> getGuessedLetters() {
+        return null;
+    }
+}

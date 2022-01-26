32ea501371c3f5fb7516660d9494d874decacd8d
Preston Thomas <preston@gmail.com>
2022-01-25 20:20:53 -0700

    makeGuess almost done

--- hangman/EvilHangman.java
+++ hangman/EvilHangman.java
@@ -2,16 +2,59 @@ package hangman;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 
+import static java.lang.Character.isLetter;
+
 public class EvilHangman {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException, EmptyDictionaryException, GuessAlreadyMadeException {
+        var game = new EvilHangmanGame();
+        Iterator<Character> itr = game.getGuessedLetters().iterator();
+        File dictionary = new File(args[0]);
+        int guesses = Integer.parseInt(args[2]);
+        int wordCount = Integer.parseInt(args[1]);
+
+        game.startGame(dictionary, wordCount);
+
+        for (int i = 0; i < guesses; i++) {
+            System.out.println("You have " + (guesses - i) + " guesses left");
+            System.out.print("Used letters: ");
+            for (char c : game.getGuessedLetters()) {
+                System.out.print(c + " ");
+            }
+
+            System.out.println("");
+            System.out.print("Word: ");
+            for (int k = 0; k < wordCount; k++) {
+                if (k == wordCount - 1) {
+                    System.out.println("-");
+                } else {
+                    System.out.print("-");
+                }
+            }
+
+            System.out.println("Enter guess: ");
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            if (!isLetter(input.charAt(0)) || input.length() != 1) {
+                System.out.println("Invalid Input");
+            }
+            else {
+                game.makeGuess(input.charAt(0));
+            }
+
 
         }
 
 
     }
+
+
+}
 
 
--- hangman/EvilHangmanGame.java
+++ hangman/EvilHangmanGame.java
@@ -2,32 +2,71 @@ package hangman;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.SortedSet;
-
+import java.util.*;
 
 
 public class EvilHangmanGame implements IEvilHangmanGame {
+
+    private SortedSet<Character> sortedSet = new TreeSet<>();
+    private Set<String> wordSet = new HashSet<>();
+
     @Override
     public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
-        Set<String> stringSet = null;
+        Set<String> words = new HashSet<>();
 
         var scanner = new Scanner(dictionary);
 
         while (scanner.hasNext()) {
             String str = scanner.next();
-            stringSet.add(str);
+            if(str.length() == wordLength) {
+                words.add(str);
+            }
         }
+        wordSet = words;
     }
 
     @Override
     public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
+        Map<String, Set<String>> setMap = new HashMap<>();
+
+        sortedSet.add(guess);
+
+        for (String s : wordSet) {
+            Set<String> subset = new HashSet<>();
+            String key = getSubsetKey(s, guess);
+            if (setMap.get(key) == null) {
+                subset.add(s);
+                setMap.put(key, subset);
+            }
+            else {
+                subset = setMap.get(key);
+                subset.add(s);
+            }
+
+
+        }
         return null;
     }
 
     @Override
     public SortedSet<Character> getGuessedLetters() {
-        return null;
+        return sortedSet;
     }
+
+    public String getSubsetKey(String s, char guess) {
+        var sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == guess) {
+                sb.append(s.charAt(i));
+            }
+            else {
+                sb.append("-");
+            }
+        }
+        return sb.toString();
+    }
+
+
+
+
 }

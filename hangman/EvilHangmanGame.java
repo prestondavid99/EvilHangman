2cc5ba8f39133cdcb954856e5a8f652bc2f35d10
prestondavid99 <buh.preston@gmail.com>
2022-01-26 14:29:58 -0700

    Test cases done

--- hangman/EvilHangmanGame.java
+++ hangman/EvilHangmanGame.java
@@ -9,28 +9,39 @@ public class EvilHangmanGame implements IEvilHangmanGame {
 
     private SortedSet<Character> sortedSet = new TreeSet<>();
     private Set<String> wordSet = new HashSet<>();
+    private int wordLength;
 
     @Override
     public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
         Set<String> words = new HashSet<>();
+        this.wordLength = wordLength;
 
-        var scanner = new Scanner(dictionary);
-
-        while (scanner.hasNext()) {
-            String str = scanner.next();
-            if(str.length() == wordLength) {
-                words.add(str);
+        try (var scanner = new Scanner(dictionary)) {
+            while (scanner.hasNext()) {
+                String str = scanner.next();
+                if (str.length() == wordLength) {
+                    words.add(str);
+                }
+            }
+            wordSet = words;
+            if (wordSet.size() == 0) {
+                throw new EmptyDictionaryException();
             }
         }
-        wordSet = words;
+
     }
 
     @Override
     public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
         Map<String, Set<String>> setMap = new HashMap<>();
         String newKey = null;
+        guess = Character.toLowerCase(guess);
+        if (sortedSet.contains(guess)) {
+            throw new GuessAlreadyMadeException();
+        }
         sortedSet.add(guess);
 
+        /* Set Making */
         for (String s : wordSet) {
             Set<String> subset = new HashSet<>();
             String key = getSubsetKey(s, guess);
@@ -42,25 +53,88 @@ public class EvilHangmanGame implements IEvilHangmanGame {
                 subset.add(s);
             }
         }
+
+        /* Finding Set With Most Values */
         int count = 0;
         for (Set<String> entry : setMap.values()) {
             if (entry.size() > count) {
                 count = entry.size();
             }
         }
-        Set<String> potentialKeys = new HashSet<>();
+        Map<String, Integer> potentialKeys = new HashMap<>();
         for (Map.Entry<String, Set<String>> entry : setMap.entrySet()) {
             if (entry.getValue().size() == count) {
                 newKey = entry.getKey();
-                potentialKeys.add(newKey);
+                potentialKeys.put(newKey, 0);
             }
         }
 
-        for (String s : potentialKeys) {
-            if (s.indexOf(guess) == -1) {
+        /* Tie Breakers */
+        for (String s : potentialKeys.keySet()) {
+            if (s.indexOf(guess) == -1) { // If the character is not found in the key
+                newKey = s;
+                wordSet = setMap.get(newKey);
+                return setMap.get(newKey);
+            }
+
+            else { // Attach letter amount to each String s
+                int letterCount = 0;
+                for (int i = 0; i < s.length(); i++) {
+                    if (s.charAt(i) == guess) {
+                        letterCount++;
+                    }
+                }
+                potentialKeys.put(s, letterCount);
+            }
+        }
+
+        int minLetterCount = wordLength;
+        for (String s : potentialKeys.keySet()) {
+            if (potentialKeys.get(s) < minLetterCount) {
+                minLetterCount = potentialKeys.get(s);
+            }
+        }
+
+        int potentialKeyCount = 0;
+        for (String s : potentialKeys.keySet()) {
+            if (potentialKeys.get(s) == minLetterCount) {
+                potentialKeyCount++;
+            }
+        }
+
+        if (potentialKeyCount == 1) { // This key has the least amount of letters
+            for (String s : potentialKeys.keySet()) {
+                if (potentialKeys.get(s) == minLetterCount) {
+                    newKey = s;
+                    wordSet = setMap.get(newKey);
+                    return setMap.get(newKey);
+                }
+            }
+        }
+
+        else {
+            for (String s : potentialKeys.keySet()) {
+                int rightCounter = 0;
+                for (int i = 0; i < s.length(); i++) {
+                    if (s.charAt(i) == guess) {
+                        rightCounter += i;
+                    }
+                }
+                potentialKeys.put(s, rightCounter);
+            }
+
+        }
+        int maxWeight = 0;
+        for (String s : potentialKeys.keySet()) {
+            if (potentialKeys.get(s) > maxWeight) {
+                maxWeight = potentialKeys.get(s);
+            }
+        }
+
+        for (String s : potentialKeys.keySet()) {
+            if (potentialKeys.get(s) == maxWeight) {
                 newKey = s;
             }
-            Map<String, Integer> countValues = new HashMap<>();
         }
         wordSet = setMap.get(newKey);
         return setMap.get(newKey);
